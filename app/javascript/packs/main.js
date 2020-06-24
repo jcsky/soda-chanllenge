6bd562863fa92bfa4058c739e490ccf3f3235efb
jcskyting <jcskyting@gmail.com>
2020-06-24 15:52:00 +0800

    item list

--- app/javascript/packs/main.js
+++ app/javascript/packs/main.js
@@ -1,5 +1,12 @@
 import Vue from 'vue/dist/vue.esm'
 import Todo from '../todo.vue'
+window.axios = require('axios');
+// import axios from 'axios';
+// import vueAxios from 'vue-axios';
+// Vue.use(vueAxios,axios);
+
+
+Vue.use(axios)
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
--- app/javascript/todo.vue
+++ app/javascript/todo.vue
@@ -1,22 +1,77 @@
 <template>
-  <div id="app">
-    <p>{{ message }}</p>
+  <div class>
+    <div class="tabs is-centered">
+      <div class="columns">
+        <div class="column">
+          <ul>
+            <li class="is-active">
+              <a>All</a>
+            </li>
+            <li>
+              <a>In progress</a>
+            </li>
+            <li>
+              <a>Completed</a>
+            </li>
+          </ul>
+          <div v-for="item in items" :key=item.id class="card">
+            <header class="card-header">
+              <p class="card-header-title">{{item.title}}</p>
+              <a href="#" class="card-header-icon" aria-label="more options">
+                <span class="icon">
+                  <i class="fas fa-angle-down" aria-hidden="true"></i>
+                </span>
+              </a>
+            </header>
+            <div class="card-content">
+              <div class="content">
+                comment: {{item.comment}}
+              </div>
+              <p>star: {{item.star}}</p>
+              <p>created_at: {{item.created_at}}</p>
+              <p>completed_at: {{item.completed_at}}</p>
+              <p>deadline_at: {{item.deadline_at}}</p>
+              <p>updated_at: {{item.updated_at}}</p>
+            </div>
+            <footer class="card-footer">
+              <a href="#" class="card-footer-item">Edit</a>
+              <a href="#" class="card-footer-item">Delete</a>
+            </footer>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import "axios";
 export default {
-  data: function () {
+  data: function() {
     return {
-      message: "Hello Vue!"
-    }
+      items: []
+    };
+  },
+  created: function() {
+    let self = this
+    axios
+      .get("/items.json")
+      .then(function(response) {
+        self.items = response.data;
+        console.log(response);
+        console.log('data')
+        console.log(self.items)
+      })
+      .catch(function(error) {
+        console.log(error);
+      })
+      .then(function() {
+        // always executed
+      });
   }
-}
+};
 </script>
 
 <style scoped>
-p {
-  font-size: 2em;
-  text-align: center;
-}
+@import "bulma/css/bulma.min.css";
 </style>
