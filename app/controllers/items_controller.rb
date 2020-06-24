fb0af61f4f5e7dd20f253f1008513f99b5812ef0
jcskyting <jcskyting@gmail.com>
2020-06-24 16:22:52 +0800

    simple create item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update, :destroy]
+  skip_before_action :verify_authenticity_token
 
   # GET /items
   # GET /items.json
--- app/javascript/todo.vue
+++ app/javascript/todo.vue
@@ -14,6 +14,31 @@
               <a>Completed</a>
             </li>
           </ul>
+
+          <!-- new -->
+
+          <div class="card">
+            <header class="card-header">
+              <h1>New item</h1>
+            </header>
+            <div class="card-content">
+              <p class="control">Title: 
+              <input v-model="newItem.title" class="input" type="text" placeholder="title">
+              </p>
+              <p class="control">Comment: 
+              <input v-model="newItem.comment" class="input" type="text" placeholder="comment">
+              </p>
+              <p>deadline_at: 
+                <input v-model="newItem.deadline_at" class="input" type="text" placeholder="comment">
+              </p>
+            </div>
+            <footer class="card-footer">
+              <button @click="saveNewItem" href="#" class="card-footer-item is-primary">Save</button>
+              <button @click="clearNewItem" href="#" class="card-footer-item is-light">Clear</button>
+            </footer>
+          </div>
+
+          <!-- list -->
           <div v-for="item in items" :key=item.id class="card">
             <header class="card-header">
               <p class="card-header-title">{{item.title}}</p>
@@ -49,25 +74,54 @@ import "axios";
 export default {
   data: function() {
     return {
-      items: []
+      items: [],
+      newItem: {
+        comment: 'new comment',
+        deadline_at: '2020-06-30T07:07:23.613Z',
+        title: ''
+      }
     };
   },
   created: function() {
-    let self = this
-    axios
-      .get("/items.json")
-      .then(function(response) {
-        self.items = response.data;
-        console.log(response);
-        console.log('data')
-        console.log(self.items)
-      })
-      .catch(function(error) {
-        console.log(error);
-      })
-      .then(function() {
-        // always executed
-      });
+    this.fetchAllItems()
+  },
+  methods: {
+    fetchAllItems(){
+      let self = this
+      axios
+        .get("/items.json")
+        .then(function(response) {
+          self.items = response.data;
+          console.log(response);
+          console.log('data')
+          console.log(self.items)
+        })
+        .catch(function(error) {
+          console.log(error);
+        })
+        .then(function() {
+          // always executed
+        });
+    },
+    saveNewItem(){
+      let self = this
+      axios
+        .post("/items", this.newItem)
+        .then(function(response) {
+          self.items = response.data;
+          self.clearNewItem()
+          self.fetchAllItems()
+        })
+        .catch(function(error) {
+          console.log(error);
+        })
+        .then(function() {
+          // always executed
+        });
+    },
+    clearNewItem(){
+      this.newItem = {title: '', comment: '', deadline_at: ''}
+    }
   }
 };
 </script>
